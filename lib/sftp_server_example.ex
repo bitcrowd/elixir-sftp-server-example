bd19045234f7f067f2e07ed50b47bb11023b80d1
Max Mulatz <max@bitcrowd.net>
2023-09-22 16:06:51 +0200

    Set up mix project
    Command:
    
    ```
    mix new --sup --app sftp_server_example .
    ```

---
+++ lib/sftp_server_example.ex
@@ -0,0 +1,18 @@
+defmodule SFTPServerExample do
+  @moduledoc """
+  Documentation for `SFTPServerExample`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SFTPServerExample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sftp_server_example_test.exs
@@ -0,0 +1,8 @@
+defmodule SFTPServerExampleTest do
+  use ExUnit.Case
+  doctest SFTPServerExample
+
+  test "greets the world" do
+    assert SFTPServerExample.hello() == :world
+  end
+end
