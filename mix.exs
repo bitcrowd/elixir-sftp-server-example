96ed75e4617020a3eb9b51d699b7e7967f60830e
Max Mulatz <max@bitcrowd.net>
2023-09-22 16:08:56 +0200

    Add ssh application
    This is a prerequisite to start Erlang's SSH daemon.

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule SFTPServerExample.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:ssh, :logger],
       mod: {SFTPServerExample.Application, []}
     ]
   end
